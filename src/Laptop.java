57caf0c8a01dbb1e4124e49b42163a153a0b1c40
Arturo Lopez <jlopezm8@upao.edu.pe>
2026-06-27 19:21:50 -0500

    feat(laptop):Implementar clases concretas.

--- src/Laptop.java
+++ src/Laptop.java
@@ -27,3 +27,41 @@ public abstract class Laptop {
                 "Tamaño de la Pantalla: " + tamanoPantalla + "\n";
     }
 }
+
+// CLASES CONCRETAS
+
+class LaptopBasica extends Laptop {
+    public LaptopBasica(String marcaYModelo, int anioFabricacion, String procesador, int tamanoPantalla) {
+        super(marcaYModelo, anioFabricacion, procesador, tamanoPantalla);
+    }
+
+    @Override
+    public void ejecutarPrueba() {
+        System.out.println("Corriendo programas en una Laptop Basica");
+        System.out.println("Laptop Basica");
+    }
+}
+
+class LaptopGamer extends Laptop {
+    public LaptopGamer(String marcaYModelo, int anioFabricacion, String procesador, int tamanoPantalla) {
+        super(marcaYModelo, anioFabricacion, procesador, tamanoPantalla);
+    }
+
+    @Override
+    public void ejecutarPrueba() {
+        System.out.println("Corriendo programas en una Laptop para Gamer");
+        System.out.println("Laptop para Gamer");
+    }
+}
+
+class LaptopProfesional extends Laptop {
+    public LaptopProfesional(String marcaYModelo, int anioFabricacion, String procesador, int tamanoPantalla) {
+        super(marcaYModelo, anioFabricacion, procesador, tamanoPantalla);
+    }
+
+    @Override
+    public void ejecutarPrueba() {
+        System.out.println("Corriendo programas en una Laptop Profesional");
+        System.out.println("Laptop para Profesional");
+    }
+}
